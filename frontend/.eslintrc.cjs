8d080a6523b09c09400ea8e4eb441b017603de84
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-08-22 01:50:39 +0200

    linting issues

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,25 +1,21 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
+  env: {
+    node: true,
+    es2022: true,
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
     'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  overrides: [
-    {
-      files: [
-        'e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'
-      ],
-      'extends': [
-        'plugin:playwright/recommended'
-      ]
-    }
+    '@vue/typescript/recommended',
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/multi-word-component-names': 'off',
+  },
 }
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,30 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>hello</h1>
   </main>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import axios from '../axiosConfig'
+
+interface Task {
+  id: number
+  name: string
+  status: boolean
+}
+
+const tasks = ref<Task[]>([])
+
+const fetchTasks = async () => {
+  try {
+    const response = await axios.get<Task[]>('/api/tasks')
+    tasks.value = response.data
+  } catch (error) {
+    console.error('error fetching list', error)
+  }
+}
+
+// Call fetchTasks when the component is mounted
+fetchTasks()
+</script>
